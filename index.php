9f1e071e8b7fa21edb7f007d5b8cd5cae545c540
issa2580 <issadiop2580@gmail.com>
2024-04-15 22:18:46 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style/bootstrap-cerulean.min.css">
+</head>
+<body>
+      <nav class="nav navbar-inverse">
+      	 <ul class="nav navbar-nav">
+		   <li><a href="#">Gestion des profiles utilisateurs</a></li>
+      	 </ul>
+      </nav>
+
+</body>
+</html> 
